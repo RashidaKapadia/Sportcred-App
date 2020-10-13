2d796893415f50c92d31b0d838aa08b06f912b4d
Jenny Quach <jennyquach.99@hotmail.com>
2020-10-12 23:22:20 -0400

    Added doc string to the requestHander class

--- backend/src/main/java/underdevelopment/api/utils/RequestHandler.java
+++ backend/src/main/java/underdevelopment/api/utils/RequestHandler.java
@@ -6,15 +6,32 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.util.HashMap;
 
+/*
+    This is a high level HttpHandler that manages multiple handlers for different 
+    request types and calls them accordingly
+*/
 public class RequestHandler implements HttpHandler {
 
     HashMap<String, HttpHandler> requestHandlers;
 
+    /**
+     * Creates a requestHandler that handles multiple request types,
+     * a minimum of one request type must be given
+     * 
+     * @param requestType the request type GET | POST | PUT | UPDATE | DELETE | ...
+     * @param handler the corresponding handler to be called when the reques is made
+     */
     public RequestHandler(String requestType, HttpHandler handler) {
         requestHandlers = new HashMap<String, HttpHandler>();
         requestHandlers.put(requestType, handler);
     }
 
+    /**
+     * Add/updates a request type
+     * 
+     * @param requestType the request type GET | POST | PUT | UPDATE | DELETE | ...
+     * @param handler the corresponding handler to be called when the reques is made
+     */
     public RequestHandler addHandler (String requestType, HttpHandler handler) {
         requestHandlers.put(requestType, handler);
         return this;
