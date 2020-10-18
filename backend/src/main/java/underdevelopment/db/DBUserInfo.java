db213eb1828c4d9eab5e8999eb139cbf61611a0a
maunicatoleti <maunicatoleti@mail.utoronto.ca>
2020-10-18 16:53:32 -0400

    changed age to dob

--- backend/src/main/java/underdevelopment/db/DBUserInfo.java
+++ backend/src/main/java/underdevelopment/db/DBUserInfo.java
@@ -1,5 +1,7 @@
-public class DBUserInfo {
+import java.util.Date;
 
+public class DBUserInfo {
+    // Variables to store user data from the signup form
     String email = "";
     String username = "";
     String password1 = "";
@@ -8,12 +10,11 @@ public class DBUserInfo {
     String sportLevel = "";
     String sportToLearn = "";
     String favTeam = "";
-    int age;
-    int acs = 100;
+    Date dob;
+    int acs = 100; // ACS initial value is 100
 
-    public DBUserInfo(String email, String username, String password, 
-    String phoneNumber, Strign favSport, String sportLevel, String sportToLearn, String favTeam, 
-    int age){
+    public DBUserInfo(String email, String username, String password, String phoneNumber, Strign favSport,
+            String sportLevel, String sportToLearn, String favTeam, Date dob) {
         this.email = email;
         this.username = username;
         this.password = password;
@@ -22,18 +23,18 @@ public class DBUserInfo {
         this.sportLevel = sportLevel;
         this.sportToLearn = sportToLearn;
         this.favTeam = favTeam;
-        this.age = age;
-        
+        this.dob = dob;
+
         try (Session session = DBConnect.driver.session()) {
-            session.writeTransaction(
-          tx -> tx.run("MERGE (a:user {username: $usr, password: $pwd, phoneNumber: $phoneNum, " + 
-          "favouriteSport: $favSport, sportLevel: $sportLevel, sportToLearn: $sportToLearn, " +
-          "favouriteTeam: $favTeam, age: $age, acs: $acs}",
-              parameters("usr", this.username, "pwd", this.password, "phoneNum", this.phoneNumber, "favSport", this.favSport,
-              "sportLevel", this.sportLevel, "sportToLearn", this.sportToLearn, "favTeam", this.favTeam, "age", this.age, "acs", this.acs)));
-        session.close();
+            session.writeTransaction(tx -> tx.run(
+                    "MERGE (a:user {username: $usr, password: $pwd, phoneNumber: $phoneNum, "
+                            + "favouriteSport: $favSport, sportLevel: $sportLevel, sportToLearn: $sportToLearn, "
+                            + "favouriteTeam: $favTeam, dateOfBirth: $dob, acs: $acs}",
+                    parameters("usr", this.username, "pwd", this.password, "phoneNum", this.phoneNumber, "favSport",
+                            this.favSport, "sportLevel", this.sportLevel, "sportToLearn", this.sportToLearn, "favTeam",
+                            this.favTeam, "dob", this.dob, "acs", this.acs)));
+            session.close();
         }
     }
-
 
 }
