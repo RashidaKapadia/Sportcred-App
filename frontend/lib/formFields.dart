ec6c64c46a7d7f1a39217990909510774d576df2
maunicatoleti <maunicatoleti@mail.utoronto.ca>
2020-10-17 22:45:20 -0400

    added date picker for selecting DOB

---
+++ frontend/lib/formFields.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+InputDecoration inputDecorator(String label, Icon icon) {
+  return InputDecoration(
+    labelText: label,
+    prefixIcon: icon,
+  );
+}
+
+String checkInput(String value, String field) {
+  if (value.isEmpty) {
+    return "Please enter your $field!";
+  }
+  return null;
+}
+
+Text txtField(String value) {
+  return Text(value, style: TextStyle(fontSize: 17, color: Colors.black54));
+}
