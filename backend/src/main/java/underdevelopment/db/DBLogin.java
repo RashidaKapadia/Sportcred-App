1a75c0e285731c5452cd97d05af969a65e01646f
Anmole Bajwa <anmoleb.bajwa@gmail.com>
2020-10-18 23:39:01 -0400

    resolved true false statement in the query

--- backend/src/main/java/underdevelopment/db/DBLogin.java
+++ backend/src/main/java/underdevelopment/db/DBLogin.java
@@ -17,11 +17,11 @@ public class DBLogin{
     // verify the user credentials
     public boolean verifyUser(String username, String password){
          try (Session session = Connect.driver.session()) {
-        	 Result result = session.run("MATCH (u:User) WHERE u.username = $x AND u.password = $y RETURN u IS NOT NULL",
+        	 Result result = session.run("MATCH (u:user) WHERE u.username = $x AND u.password = $y RETURN u IS NOT NULL",
              parameters("x",  username, "y", password));
         		if (result.hasNext()) {
         			//System.out.println("This user is registered");      
-        			return false; 
+        			return true; 
         		}
         	return false; 
         }
