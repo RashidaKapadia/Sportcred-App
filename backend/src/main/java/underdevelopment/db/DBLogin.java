2953e7d56383a4ca466694f805380eddc325b98c
Anmole Bajwa <anmoleb.bajwa@gmail.com>
2020-10-18 23:31:18 -0400

    resolved an issue in cypher query

--- backend/src/main/java/underdevelopment/db/DBLogin.java
+++ backend/src/main/java/underdevelopment/db/DBLogin.java
@@ -17,7 +17,7 @@ public class DBLogin{
     // verify the user credentials
     public boolean verifyUser(String username, String password){
          try (Session session = Connect.driver.session()) {
-        	 Result result = session.run("MATCH (u:User) WHERE u.username = $x AND u.password = $y RETURN p IS NOT NULL",
+        	 Result result = session.run("MATCH (u:User) WHERE u.username = $x AND u.password = $y RETURN u IS NOT NULL",
              parameters("x",  username, "y", password));
         		if (result.hasNext()) {
         			//System.out.println("This user is registered");      
