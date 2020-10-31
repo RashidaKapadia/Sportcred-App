a4bafb9601801edb0557b05b091142adb2002581
Thisiszhihua@gmail.com <thisiszhihua@gmail.com>
2020-10-31 14:30:44 -0400

    UN-54 adding changes to take into account ACS being stored as a string for some reason....

--- backend/src/main/java/underdevelopment/db/DBAcs.java
+++ backend/src/main/java/underdevelopment/db/DBAcs.java
@@ -32,10 +32,10 @@ public class DBAcs {
 		 try (Session session = Connect.driver.session()) {
 	    	try (Transaction tx = session.beginTransaction()) {
 	    		// Update the person's ACS score
-				Result result = tx.run(String.format("MATCH (n { username: '%s' }) SET n.acs = %d + n.acs  RETURN n.username as username, n.acs as acs", username, ammount));
+				Result result = tx.run(String.format("MATCH (n { username: '%s' }) SET n.acs = %d + toInteger(n.acs)  RETURN n.username as username, n.acs as acs", username, ammount));
 				//Result result = tx.run("MATCH (n { username: 'banana420' }) SET n.acs = 5556  RETURN n.username as username, n.acs as acs");
 	    		record = result.next();
-	    		newACS = Integer.parseInt(record.get("acs").asString());
+	    		newACS = record.get("acs").asInt();
     			
 	    		
 	    		System.out.println("checking");
