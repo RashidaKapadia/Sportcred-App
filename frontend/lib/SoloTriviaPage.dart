46aab5c6218eba9d493d31dd58133b9a8dcf6752
maunicatoleti <maunicatoleti@mail.utoronto.ca>
2020-11-01 16:04:29 -0500

    UN-63 Added code for making http request to get questions from backend, created TrviaQuestions class to format them and created class variables to store the questions, options and correct answers.

--- frontend/lib/SoloTriviaPage.dart
+++ frontend/lib/SoloTriviaPage.dart
@@ -1,9 +1,65 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:frontend/TriviaResult.dart';
 import 'onGoingTrivia.dart';
 import './navbar.dart';
 import 'dart:async';
+import 'package:http/http.dart' as http;
+
+class TriviaQuestions {
+  final Map<String, String> questions;
+  final Map<String, List<String>> options;
+  final Map<String, String> correctAnswers;
+  final bool reqStatus;
+
+  TriviaQuestions(
+      {this.questions,
+      this.options,
+      this.correctAnswers,
+      @required this.reqStatus});
+
+  // converts json to TriviaQuestions object
+  factory TriviaQuestions.fromJson(bool status, List<dynamic> json) {
+    // If there is an error
+    if (!status && json == null) {
+      return TriviaQuestions(
+        reqStatus: status,
+      );
+    }
+
+    // Initialize maps for storing questions, their options and correct answers
+    Map<String, String> qs;
+    Map<String, List<String>> opts;
+    Map<String, String> correct;
+    int n = 0;
+
+    for (dynamic q in json) {
+      // Get the question
+      qs[n.toString()] = q['question'];
+
+      // get the options for current question
+      opts[n.toString()] = [
+        q['answer'],
+        q['otherChoices'][0],
+        q['otherChoices'][1],
+        q['otherChoices'][2]
+      ];
+
+      // get the answer
+      correct[n.toString()] = q['answer'];
+
+      // Increment n
+      n++;
+    }
+
+    return TriviaQuestions(
+        reqStatus: status,
+        questions: qs,
+        options: opts,
+        correctAnswers: correct);
+  }
+}
 
 class SoloTriviaPage extends StatefulWidget {
   @override
@@ -22,7 +78,74 @@ class _TriviaState extends State<SoloTriviaPage> {
   // hardcoded to be Basketball for now
   String chosenCategory = 'Basketball';
 
+  // Trivia questions, options and correct answers
+  Map<String, String> triviaQuestions;
+  Map<String, List<String>> triviaOptions;
+  Map<String, String> triviaAnswers;
+
   Timer _timer;
+
+  Future<TriviaQuestions> _futureTriviaQuestions;
+
+  // Http post request to get user info
+  Future<TriviaQuestions> getQuestions(String category) async {
+    // Make the request and store the response
+    final http.Response response = await http.post(
+      'http://localhost:8080/api/trivia/get-questions',
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Accept': 'text/plain; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: jsonEncode(<String, String>{'category': category}),
+    );
+
+    if (response.statusCode == 200) {
+      // Store the session token
+      print("PROFILE GET -> RESPONSE:" + response.body.toString());
+
+      TriviaQuestions triviaData =
+          TriviaQuestions.fromJson(true, jsonDecode(response.body));
+
+      setState(() {
+        // Get the questions, options and correctAnswers and store them in the class variables
+        this.triviaQuestions = triviaData.questions;
+        this.triviaOptions = triviaData.options;
+        this.triviaAnswers = triviaData.correctAnswers;
+
+        // DEBUGGING STATEMENTS
+        print('DEBUGGING: TRIVIA GET QUESTIONS');
+        print(triviaQuestions);
+        print(triviaOptions);
+        print(triviaAnswers);
+      });
+
+      // Return trivia data
+      return triviaData;
+    } else {
+      return TriviaQuestions(reqStatus: false);
+    }
+    return null;
+  }
+
+  /// Getters for the trivia questions, options and correct answers
+  Map<String, String> getTriviaQuestions() {
+    print(triviaQuestions);
+
+    return this.triviaQuestions;
+  }
+
+  Map<String, List<String>> getTriviaOptions() {
+    print(triviaOptions);
+
+    return this.triviaOptions;
+  }
+
+  Map<String, String> getTriviaAnswers() {
+    print(triviaAnswers);
+
+    return this.triviaAnswers;
+  }
 
   Future<Widget> DialogBox(BuildContext context) async {
     showDialog(
