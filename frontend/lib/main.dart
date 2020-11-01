a2805089d9648da4c9809134e485312d0be5691a
Anmole Bajwa <anmoleb.bajwa@gmail.com>
2020-11-01 15:21:34 -0500

    Splash screen causing some errors for navigation to SignUp page so removing it for now

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -36,6 +36,6 @@ class _SportCredAppState extends State<SportCredApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return InitialPage();
+    return LoginPage();
   }
 }
