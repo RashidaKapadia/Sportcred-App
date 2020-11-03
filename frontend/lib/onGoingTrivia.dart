b0ff6208cd701c7991d555afdb4180da1771222e
Anmole Bajwa <anmoleb.bajwa@gmail.com>
2020-11-02 20:44:29 -0500

    removed imports not being used

--- frontend/lib/onGoingTrivia.dart
+++ frontend/lib/onGoingTrivia.dart
@@ -1,16 +1,13 @@
 // Reference:
 // flutter-quizstar. https://github.com/desi-programmer/flutter-quizstar
 
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:frontend/TriviaResult.dart';
 import 'dart:async';
 import 'dart:math';
-import 'package:frontend/homepage.dart';
 import './SoloTriviaPage.dart';
 import 'package:simple_timer/simple_timer.dart';
-import 'package:flutter/animation.dart';
 
 class OnGoingTrivia extends StatelessWidget {
   String category;
@@ -85,16 +82,7 @@ class _quizpageState extends State<quizPage> with TickerProviderStateMixin {
   var randomList;
   TimerController _timerController;
 
-  /*Map<int, Color> colorsForOptions = {
-    0: Colors.indigoAccent,
-    1: Colors.indigoAccent,
-    2: Colors.indigoAccent,
-    3: Colors.indigoAccent
-  };*/
-
   bool cancelTimer = false;
-
-  colorOptionsReset() {}
 
   createRandomList() {
     var distinctIds = [];
@@ -172,10 +160,6 @@ class _quizpageState extends State<quizPage> with TickerProviderStateMixin {
                       10 - (pressedIncorrectOption + pressedCorrectOption))));
         });
       }
-      //colorsForOptions[0] = Colors.indigoAccent;
-      //colorsForOptions[1] = Colors.indigoAccent;
-      //colorsForOptions[2] = Colors.indigoAccent;
-      //colorsForOptions[3] = Colors.indigoAccent;
       disableAnswer = false;
       _isPressedCorrect = -1;
     });
@@ -198,18 +182,15 @@ class _quizpageState extends State<quizPage> with TickerProviderStateMixin {
         pressedIncorrectOption = pressedIncorrectOption + 1;
       }
       setState(() {
-        //colorsForOptions[t] = colorToDisplay;
         cancelTimer = true;
         disableAnswer = true;
         _isPressed = false;
       });
     } else {
-      //notAnswered = notAnswered + 1;
       marks = marks - 1;
     }
 
     Timer(Duration(seconds: 1), nextQuestion);
-    //return _isCorrect;
   }
 
   Widget answersAnimation(int t) {
@@ -227,12 +208,11 @@ class _quizpageState extends State<quizPage> with TickerProviderStateMixin {
             data[i].options[t],
             maxLines: 1,
           ),
-          //color: colorsForOptions[t],
           color: _isPressedCorrect == 0
               ? Colors.green
               : _isPressedCorrect == 1
                   ? Colors.red
-                  : Colors.indigoAccent, //Colors.indigoAccent,
+                  : Colors.indigoAccent,
           highlightColor: Colors.indigo[700],
           minWidth: 200.0,
           height: 45.0,
@@ -282,23 +262,18 @@ class _quizpageState extends State<quizPage> with TickerProviderStateMixin {
             Expanded(
               flex: 3,
               child: Container(
-                //width: 200,
-                //height: 20,
                 alignment: Alignment.center,
                 padding: EdgeInsets.all(20.0),
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.indigoAccent, width: 5.0),
-                  //shape: BoxShape.circle,
                   borderRadius: BorderRadius.all(Radius.circular(50)),
                 ),
-
                 child: Text(
                   'Q. ' + data[i].question,
                   style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
                 ),
               ),
             ),
-            //SizedBox(height: 100.0),
             Expanded(
               flex: 6,
               child: AbsorbPointer(
@@ -306,22 +281,9 @@ class _quizpageState extends State<quizPage> with TickerProviderStateMixin {
                 child: Container(
                   child: Wrap(
                       direction: Axis.vertical,
-                      // spacing: 8.0, // gap between adjacent chips
-                      //runSpacing: 4.0,
-                      //mainAxisAlignment: MainAxisAlignment.center,
                       children: List.generate(data[i].options.length, (index) {
-                        //colorsForOptions[index] = Colors.indigoAccent;
                         return answersAnimation(index);
-                      })
-                      //children: List.generate(data[i].length(options), index){
-                      //return answersAnimation(index);
-                      // },
-                      //answersAnimation(0),
-                      //answersAnimation(1),
-                      //answersAnimation(2),
-                      //answersAnimation(3),
-
-                      ),
+                      })),
                 ),
               ),
             ),
