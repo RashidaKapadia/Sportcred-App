a3d1d82a48d6fd0168eefdd5d8bc8971ca9e4e97
jenny quach <jennyquach.99@hotmail.com>
2020-11-19 00:28:20 -0500

    UN 131: Added in multiplayer http calls

--- frontend/lib/requests/trivia.dart
+++ frontend/lib/requests/trivia.dart
@@ -1,6 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+var defaultHeaders = {
+  'Content-Type': 'text/plain; charset=utf-8',
+  'Accept': 'text/plain; charset=utf-8',
+  'Access-Control-Allow-Origin': '*',
+};
+
 class TriviaQuestion {
   final String question;
   final List<dynamic> options;
@@ -22,17 +28,12 @@ Future<List<TriviaQuestion>> getQuestions(String category) async {
   // Make the request and store the response
   final http.Response response = await http.post(
     'http://localhost:8080/api/trivia/get-questions',
-    headers: {
-      'Content-Type': 'text/plain; charset=utf-8',
-      'Accept': 'text/plain; charset=utf-8',
-      'Access-Control-Allow-Origin': '*',
-    },
+    headers: defaultHeaders,
     body: jsonEncode(<String, String>{'category': category}),
   );
 
   if (response.statusCode == 200) {
     List<TriviaQuestion> triviaQs = [];
-
     // Get the questions, options and correctAnswers and store them in the class variables
     for (Map<String, dynamic> question
         in jsonDecode(response.body)["questions"] as List) {
@@ -49,11 +50,7 @@ Future updateACS(String token, String username, int score) async {
   // Make the request and store the response
   final http.Response response =
       await http.post('http://localhost:8080/api/editACS"',
-          headers: {
-            'Content-Type': 'text/plain; charset=utf-8',
-            'Accept': 'text/plain; charset=utf-8',
-            'Access-Control-Allow-Origin': '*',
-          },
+          headers: defaultHeaders,
           body: jsonEncode(<String, String>{
             "username": username,
             "token": token,
@@ -64,5 +61,55 @@ Future updateACS(String token, String username, int score) async {
           }));
 
   // Check the type of response received from backend
+  return (response.statusCode == 200);
+}
+
+Future startMultiplayerTrivia(String username, String opponent) async {
+  final http.Response response =
+      await http.post('http://localhost:8080/api/trivia/start-multiplayer-game',
+          headers: defaultHeaders,
+          body: jsonEncode(<String, String>{
+            "username": username,
+            "opponent": opponent,
+          }));
+
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body)['gameID'];
+  }
+  return -1;
+}
+
+Future joinMultiplayerTrivia(int gameId) async {
+  final http.Response response =
+      await http.post('http://localhost:8080/api/trivia/join-multiplayer-game',
+          headers: defaultHeaders,
+          body: jsonEncode(<String, dynamic>{
+            "gameID": gameId,
+          }));
+
+  List<TriviaQuestion> triviaQs = [];
+  String opponent = "<unknown>";
+
+  if (response.statusCode == 200) {
+    Map<String, dynamic> json = jsonDecode(response.body);
+    opponent = json["inviter"];
+    for (Map<String, dynamic> question in json["questions"] as List) {
+      triviaQs.add(TriviaQuestion.fromJson(question));
+    }
+  }
+  return [triviaQs, opponent];
+}
+
+Future endMultiplayerTrivia(
+    String username, int gameId, List<String> answers, int gameScore) async {
+  final http.Response response =
+      await http.post('http://localhost:8080/api/trivia/end-multiplayer-game',
+          headers: defaultHeaders,
+          body: jsonEncode(<String, dynamic>{
+            "gameID": gameId,
+            "username": username,
+            "answers": answers,
+            "gameScore": gameScore
+          }));
   return (response.statusCode == 200);
 }
