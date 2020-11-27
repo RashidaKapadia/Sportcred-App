8026594c39c0306cd30e62315e7d16c88f7105fa
maunicatoleti <maunicatoleti@mail.utoronto.ca>
2020-11-26 21:42:35 -0500

    UN-192 - Created a node class to represent a daily debate question, created a method to call the backend API to getDailyDebateQuestion

--- frontend/lib/debate/dailyDebateQuestion.dart
+++ frontend/lib/debate/dailyDebateQuestion.dart
@@ -8,23 +8,85 @@ import 'package:frontend/widgets/layout.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
+class DebateQuestionNode {
+  final String question;
+  final int id;
+  final bool reqStatus;
+
+  DebateQuestionNode({this.question, this.id, this.reqStatus});
+
+  // converts json to post node object
+  factory DebateQuestionNode.fromJson(bool status, Map<String, dynamic> json) {
+    if (json == null) {
+      return DebateQuestionNode(
+        reqStatus: status,
+      );
+    }
+
+    return DebateQuestionNode(
+      reqStatus: status,
+      question: json["dailyQuestion"].toString(),
+      id: int.parse(json["id"]),
+    );
+  }
+}
+
 class DailyDebateQuestion extends StatefulWidget {
   @override
   _DailyDebateQuestionState createState() => _DailyDebateQuestionState();
 }
 
 class _DailyDebateQuestionState extends State<DailyDebateQuestion> {
-  String question = "";
   String response = "";
+  String currentUser = "";
+  DebateQuestionNode dailyQuestion;
+
+  Future<DebateQuestionNode> getDailyQuestion() async {
+    // Make the request and store the response
+    final http.Response response = await http.post(
+      'http://localhost:8080/api/getDailyDebateQuestion',
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Accept': 'text/plain; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: jsonEncode(<String, String>{'username': currentUser}),
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        print(jsonDecode(response.body));
+
+        dailyQuestion =
+            DebateQuestionNode.fromJson(true, jsonDecode(response.body));
+        
+      });
+
+      print("*********************");
+      print(dailyQuestion.question);
+      print(dailyQuestion.id);
+      print("*********************");
+
+      return dailyQuestion;
+      // Return posts data
+    } else {
+      return null;
+    }
+  }
 
   @override
   void initState() {
     super.initState();
 
     setState(() {
-      question = "How are you?";
+      // Get question
+      getDailyQuestion();
+      // get current user's username
+      FlutterSession()
+          .get('username')
+          .then((username) => {currentUser = username.toString()});
     });
-    // Get question
+    print(currentUser);
   }
 
   @override
@@ -36,30 +98,30 @@ class _DailyDebateQuestionState extends State<DailyDebateQuestion> {
           title: Text("Daily Debate Question",
               style: TextStyle(color: Colors.white)),
         ),
-        body: SingleChildScrollView(child: margin20(
-           Column(
-            children: [
-              vmargin25(Text(question, style: TextStyle(fontSize: 20))),
-              vmargin20(hmargin15(TextField(
-                  style: TextStyle(fontSize: 16),
-                  cursorColor: Colors.orange,
-                  decoration: InputDecoration(
-                      hintText: "Response",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5))),
-                  onChanged: (value) {
-                    setState(() {
-                      response = value;
-                    });
-                  },
-                  keyboardType: TextInputType.multiline,
-                  maxLines: null))),
-                  vmargin25(orangeButtonLarge(text: "Submit", 
-                  onPressed: (){
-                    // call send response API
-                  })),
-            ],
-          )
-    )));
+        body: SingleChildScrollView(
+            child: margin20(Column(
+          children: [
+            vmargin25(Text(dailyQuestion == null ? "" : dailyQuestion.question, style: TextStyle(fontSize: 20))),
+            vmargin20(hmargin15(TextField(
+                style: TextStyle(fontSize: 16),
+                cursorColor: Colors.orange,
+                decoration: InputDecoration(
+                    hintText: "Response",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5))),
+                onChanged: (value) {
+                  setState(() {
+                    response = value;
+                  });
+                },
+                keyboardType: TextInputType.multiline,
+                maxLines: null))),
+            vmargin25(orangeButtonLarge(
+                text: "Submit",
+                onPressed: () {
+                  // call send response API
+                })),
+          ],
+        ))));
   }
 }
