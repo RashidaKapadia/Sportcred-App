e8cb528eafa297c2059d1ccf783a87a5ee880418
maunicatoleti <maunicatoleti@mail.utoronto.ca>
2020-11-29 15:05:21 -0500

    UN-161 Created methods to create response node and add relationship between a question and a response

--- backend/src/main/java/underdevelopment/db/DBDebateDailyQuestion.java
+++ backend/src/main/java/underdevelopment/db/DBDebateDailyQuestion.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import org.neo4j.driver.Record;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+
 import static org.neo4j.driver.Values.parameters;
 
 
@@ -21,7 +23,7 @@ public class DBDebateDailyQuestion {
         try (Session session = Connect.driver.session()) {
             // NEED TO UPDATE DATE TO LOCAL DATE AFTER DEC 1
             Result result = session.run("MATCH (q:DebateQuestion {tier: $t, date: $d} ) RETURN q",
-                                        parameters("t", tier, "d", "2020-12-01"));
+                                        parameters("t", tier, "d", "2020-12-02"));
             
             if (result.hasNext()){
                 Record r = result.single();
@@ -37,5 +39,43 @@ public class DBDebateDailyQuestion {
             return null;
         }
         return null;
+    }
+
+    public static int addResponse(String username, String analysis){
+        try (Session session = Connect.driver.session()) {
+
+            Result result = session.run("MERGE (r:DebateResponse {username: $u, debateAnalysis: $a} ) RETURN ID(r) as id",
+                                        parameters("u", username, "a", analysis));
+            
+            if (result.hasNext()){
+                Record r = result.next();
+
+                int id = r.get("id").asInt();
+                return id;
+
+            }
+            return -1;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public static boolean addResponseToQuestion(String tier, int id){
+        try (Session session = Connect.driver.session()) {
+            Transaction tx = session.beginTransaction();
+            
+            // NEED TO CHANGE DATE TO LocalDate.now().toString()
+            tx.run("MATCH (q:DebateQuestion {tier: $t, date: $d}), (r:DebateResponse) " 
+            + "WHERE ID(r) = $i MERGE (q)-[:hasResponse]->(r) RETURN r",
+                                        parameters("t", tier, "d", "2020-12-01", "i", id));
+            
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
