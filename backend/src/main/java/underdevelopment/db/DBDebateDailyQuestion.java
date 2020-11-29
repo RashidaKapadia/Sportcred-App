0f07605a40849076a393e39e71807a894e4c9120
maunicatoleti <maunicatoleti@mail.utoronto.ca>
2020-11-28 21:41:05 -0500

    UN-192 Added method to query and get dailyQuestion from DB, used the method in DBDebateDailyQuestion to get daily question in DebateDailyQuestion.java and fixed typo in tier names in debateQuestions.json

--- backend/src/main/java/underdevelopment/db/DBDebateDailyQuestion.java
+++ backend/src/main/java/underdevelopment/db/DBDebateDailyQuestion.java
@@ -1,7 +1,41 @@
 package underdevelopment.db;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import static org.neo4j.driver.Values.parameters;
+
+
 public class DBDebateDailyQuestion {
 
      
+    public static Map<String, String> getDailyDebateQuestion(String tier){
 
+        System.out.println(LocalDate.now().toString());
+
+        try (Session session = Connect.driver.session()) {
+            // NEED TO UPDATE DATE TO LOCAL DATE AFTER DEC 1
+            Result result = session.run("MATCH (q:DebateQuestion {tier: $t, date: $d} ) RETURN q",
+                                        parameters("t", tier, "d", "2020-12-01"));
+            
+            if (result.hasNext()){
+                Record r = result.single();
+
+                Map<String, String> question = new HashMap<>();
+                question.put("question", r.get("q").get("question").asString());
+
+                return question;
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
 }
