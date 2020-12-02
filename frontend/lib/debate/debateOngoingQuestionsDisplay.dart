6ae2c82bc2b6e2289b59ea49eead0bcc934de87e
Anmole Bajwa <anmoleb.bajwa@gmail.com>
2020-12-02 13:35:47 -0500

    UN-179: added client call for getting questions

--- frontend/lib/debate/debateOngoingQuestionsDisplay.dart
+++ frontend/lib/debate/debateOngoingQuestionsDisplay.dart
@@ -1,11 +1,15 @@
+import 'dart:convert';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_session/flutter_session.dart';
 import 'package:frontend/widgets/buttons.dart';
 import 'package:frontend/widgets/layout.dart';
 import '../navbar.dart';
 import 'package:flutter_cursor/flutter_cursor.dart';
+import 'package:http/http.dart' as http;
 
 class CurrentDebateQuestions extends StatefulWidget {
   @override
@@ -33,9 +37,11 @@ final tiers = ["Expert", "Pro Analyst", "Analyst", "Fanalyst"];
 class QuestionNode {
   final String questionId;
   final String question;
+  final String tier;
   final bool reqStatus;
 
-  QuestionNode({this.questionId, this.question, @required this.reqStatus});
+  QuestionNode(
+      {this.questionId, this.question, this.tier, @required this.reqStatus});
 
   // converts json to UserInfo object
   factory QuestionNode.fromJson(bool status, Map<String, dynamic> json) {
@@ -48,11 +54,15 @@ class QuestionNode {
     return QuestionNode(
         reqStatus: status,
         questionId: json['questionId'],
+        tier: json['tier'],
         question: json['question']);
   }
 }
 
 Widget displayTiers(int i, BuildContext context) {
+  if (questionsList.isEmpty) {
+    print("It is empty");
+  }
   switch (i) {
     case 0:
       {
@@ -76,7 +86,7 @@ Widget displayTiers(int i, BuildContext context) {
                 textColor: Colors.white,
                 child: Column(children: [
                   Text(tiers[i], style: TextStyle(fontSize: 15)),
-                  AutoSizeText("\nQuestion")
+                  AutoSizeText("\n" + questionsList[i].question)
                 ]),
               ),
             ),
@@ -106,7 +116,7 @@ Widget displayTiers(int i, BuildContext context) {
                 textColor: Colors.white,
                 child: Column(children: [
                   Text(tiers[i], style: TextStyle(fontSize: 15)),
-                  AutoSizeText("\nQuestion")
+                  AutoSizeText("\n" + questionsList[1].question)
                 ]),
               ),
             ),
@@ -135,7 +145,7 @@ Widget displayTiers(int i, BuildContext context) {
                       textColor: Colors.white,
                       child: Column(children: [
                         Text(tiers[i], style: TextStyle(fontSize: 15)),
-                        AutoSizeText("\nQuestion")
+                        AutoSizeText("\n" + questionsList[2].question)
                       ]),
                     ))));
       }
@@ -162,7 +172,7 @@ Widget displayTiers(int i, BuildContext context) {
               textColor: Colors.white,
               child: Column(children: [
                 Text(tiers[i], style: TextStyle(fontSize: 15)),
-                AutoSizeText("\nQuestion")
+                AutoSizeText("\n" + questionsList[3].question)
               ]),
             ),
           ),
@@ -177,10 +187,70 @@ Widget displayTiers(int i, BuildContext context) {
   }
 }
 
+List<QuestionNode> questionsList = [];
+
 class _CurrentDebateQuestionsState extends State<CurrentDebateQuestions> {
   List data;
+  String currentUser = '';
+  //Future<QuestionNode> _futureQuestionNode;
+  Future<List<QuestionNode>> _futureQuestions;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _futureQuestions = getQuestions();
+      print("FUTURE QUESTIONS" + _futureQuestions.toString());
+      print("init" + questionsList.toString());
+
+      FlutterSession()
+          .get('username')
+          .then((username) => {currentUser = username.toString()});
+    });
+  }
+
+  Future<List<QuestionNode>> getQuestions() async {
+    // Make the request and store the response
+    final http.Response response = await http.post(
+      'http://localhost:8080/api/debate/get-ongoing-questions',
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Accept': 'text/plain; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: jsonEncode(<String, String>{}),
+    );
+
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      List<QuestionNode> allQuestions = [];
+      // Get the questions, options and correctAnswers and store them in the class variables
+      for (Map<String, dynamic> questionNode
+          in jsonDecode(response.body)["questions"] as List) {
+        print("*********************");
+        // print(QuestionNode.fromJson(true, questionNode).tier);
+        print("*********************");
+
+        allQuestions += [QuestionNode.fromJson(true, questionNode)];
+        print(allQuestions[0].tier);
+      }
+      // DEBUGGING STATEMENTS
+      print('DEBUGGING: Post Node Get');
+      print("\n\nQuestionodes: " + allQuestions[0].question);
+      print(allQuestions.length);
+      setState(() {
+        questionsList = allQuestions;
+        print("in api" + questionsList.toString());
+      });
+      return allQuestions;
+      // Return posts data
+    } else {
+      return null;
+    }
+  }
+
   Widget build(BuildContext context) {
-    //final appState = AppStateProvider.of<AppState>(context);
+    //
     return Scaffold(
         appBar: AppBar(
             leading: BackButton(
