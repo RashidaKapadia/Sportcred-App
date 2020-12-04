ee727cf714dcf5f0d11fa2e0b32c81d22923e7d8
Anmole Bajwa <anmoleb.bajwa@gmail.com>
2020-12-03 19:26:34 -0500

    UN-167: Fixing the client call for debateVote

--- frontend/lib/requests/debate.dart
+++ frontend/lib/requests/debate.dart
@@ -8,7 +8,7 @@ var defaultHeaders = {
   'Access-Control-Allow-Origin': '*',
 };
 
-class GroupNode {
+/*class GroupNode {
   final String groupId;
   List<Group_ResponsesNode> responses;
   final bool reqStatus;
@@ -30,8 +30,31 @@ class GroupNode {
     );
   }
 }
+*/
 
-class Group_ResponsesNode {
+class GroupNode {
+  final String groupId;
+  List<dynamic> responses;
+
+  final bool reqStatus;
+
+  GroupNode({this.groupId, this.responses, this.reqStatus});
+
+  // converts json to UserInfo object
+  factory GroupNode.fromJson(bool status, Map<String, dynamic> json) {
+    if (json == null) {
+      return GroupNode(
+        reqStatus: status,
+      );
+    }
+
+    return GroupNode(
+        reqStatus: status,
+        groupId: json['groupId'],
+        responses: json['responses']);
+  }
+}
+/*class Group_ResponsesNode {
   final String response;
   final int responseId;
   final bool reqStatus;
@@ -48,12 +71,133 @@ class Group_ResponsesNode {
     return Group_ResponsesNode(
       reqStatus: status,
       response: json['response'],
-      responseId: json['groupId'],
+      responseId: json['responseId'],
     );
   }
 }
+*/
 
-Future<List<GroupNode>> getGroupResponses(String username) async {
+class ResponseNode {
+  final int responseId;
+  final String response;
+  final bool reqStatus;
+
+  ResponseNode({this.responseId, this.response, this.reqStatus});
+
+  // converts json to UserInfo object
+  factory ResponseNode.fromJson(bool status, Map<String, dynamic> json) {
+    if (json == null) {
+      return ResponseNode(
+        reqStatus: status,
+      );
+    }
+
+    return ResponseNode(
+        reqStatus: status,
+        responseId: json['responseId'],
+        response: json['response']);
+  }
+}
+
+List<GroupNode> makeGroupResponseList(List<dynamic> list) {
+  List<GroupNode> gr = [];
+  for (Map<String, dynamic> group in list) {
+    gr.add(GroupNode.fromJson(true, group));
+  }
+  return gr;
+}
+
+List<ResponseNode> makeRepsonseList(List<dynamic> list) {
+  List<ResponseNode> qs = [];
+  for (Map<String, dynamic> response in list) {
+    qs.add(ResponseNode.fromJson(true, response));
+  }
+  return qs;
+}
+
+Future<List<dynamic>> getDebateVoteData(String username) async {
+  return [getMyQuestion(username), getVoteGroupResponses(username)];
+}
+
+Future<String> getMyQuestion(String username) async {
+  // Make the request and store the response
+  final http.Response response = await http.post(
+    'http://localhost:8080/api/debate/get-daily-question',
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+      'Accept': 'text/plain; charset=utf-8',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: jsonEncode(<String, Object>{
+      "username": username,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    print(response.statusCode);
+    return jsonDecode(response.body)["dailyQuestion"];
+  } else {
+    print(response.statusCode);
+    return null;
+  }
+}
+
+Future<List<GroupNode>> getVoteGroupResponses(String username) async {
+  // Make the request and store the response
+  final http.Response response = await http.post(
+    'http://localhost:8080/api/debate/get-group-responses',
+    headers: defaultHeaders,
+    body: jsonEncode(<String, Object>{
+      "username": username,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    print(response.statusCode);
+    List<GroupNode> r =
+        makeGroupResponseList(jsonDecode(response.body)["groups"]);
+
+    for (int i = 0; i < r.length; i++) {
+      List<ResponseNode> res = makeRepsonseList(r[i].responses);
+      print("before" + r[i].responses.toString());
+      print(res[0].response);
+
+      r[i].responses = res;
+      print("after" + r[i].responses.toString());
+      print(r[i].responses[0].response);
+    }
+    print(r[0].responses);
+    return r;
+  } else {
+    print(response.statusCode);
+    return null;
+  }
+}
+
+/*Future<List<GroupNode>> getGroupResponses(String username) async {
+  // Make the request and store the response
+  final http.Response response = await http.post(
+    'http://localhost:8080/api/debate/get-group-responses-my-question',
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+      'Accept': 'text/plain; charset=utf-8',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: jsonEncode(<String, Object>{
+      "username": username,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    print(response.statusCode);
+    return makeGroupResponsesList(jsonDecode(response.body)["groups"]);
+  } else {
+    print(response.statusCode);
+    return null;
+  }
+}*/
+
+/*Future<List<GroupNode>> getGroupResponses(String username) async {
   final http.Response response = await http.post(
       'http://localhost:8080/api/debate/get-group-responses-my-question',
       headers: defaultHeaders,
@@ -92,7 +236,7 @@ Future<List<GroupNode>> getGroupResponses(String username) async {
   } else {
     return null;
   }
-}
+}*/
 
 /**
  * Submits the votes for the debate group
