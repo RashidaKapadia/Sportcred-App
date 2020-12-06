e10ffeb5c05b84cc704c9e4ee8a8b297ddd3bb36
maunicatoleti <maunicatoleti@mail.utoronto.ca>
2020-12-06 14:35:46 -0500

    Changed username to user's first name and acs score.

--- frontend/lib/homepage.dart
+++ frontend/lib/homepage.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_session/flutter_session.dart';
 import 'package:frontend/widgets/layout.dart';
 import './navbar.dart';
 import 'package:frontend/widgets/fonts.dart';
+import 'package:frontend/profile_page.dart';
+import 'package:http/http.dart' as http;
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,19 +16,59 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var isSelected = false;
-  String username = "";
+  String name = "";
+  String acs = "";
+
+ // Http post request to get user info
+  Future<UserInfo> getUserInfo(String username, String token) async {
+    // Make the request and store the response
+    final http.Response response = await http.post(
+      'http://localhost:8080/api/getUserInfo',
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Accept': 'text/plain; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: jsonEncode(<String, String>{'username': username, 'token': token}),
+    );
+
+    if (response.statusCode == 200) {
+      UserInfo userData = UserInfo.fromJson(true, jsonDecode(response.body));
+
+      setState(() {
+        this.name = userData.firstname;
+        this.acs = userData.acs.toString();
+       
+        print(this.name);
+
+        storePrevValues();
+      });
+
+      return userData;
+    } else if (response.statusCode == 403) {
+      Navigator.of(context).pushNamed('/login');
+      return UserInfo(reqStatus: false);
+    } else {
+      return UserInfo(reqStatus: false);
+    }
+  }
 
   void loadUsername() {
-    FlutterSession().get('username').then((value) {
-      this.setState(() {
-        username = value.toString();
-      });
+     FlutterSession().get('token').then((token) {
+      FlutterSession().get('username').then((username) => {
+            setState(() {
+              String store_token = token.toString();
+               getUserInfo(username.toString(), store_token);
+            })
+          });
     });
+   
   }
 
   @override
   void initState() {
     super.initState();
+
     loadUsername();
   }
 
@@ -62,7 +107,7 @@ class _HomePageState extends State<HomePage> {
                         padding: EdgeInsets.all(20),
                         color: Color.fromRGBO(31, 38, 5, 0.9),
                         child: Text(
-                          "Welcome " + username + "!",
+                          "Welcome " + name + " (ACS: " + acs + ")" + "!",
                           style: TextStyle(
                               fontWeight: FontWeight.bold,
                               color: Color.fromRGBO(255, 255, 255, 0.9)),
